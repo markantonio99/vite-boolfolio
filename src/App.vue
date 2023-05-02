d00879f82fe394ac6b989fc11b8ff1bd139493fd
“markantonio99” <markomakkia@gmail.com>
2023-05-03 00:58:10 +0200

    salv 002

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<script>
+import Events from './components/Event.vue'
+
+export default ({
+  created () {
+  },
+  components: {
+    Events
+  }
+})
+
+</script>
+
+<template>
+  <div class="container">
+    <Events />
+  </div>
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
---
+++ src/components/event.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <p v-for="event in events" :key="event.id">{{ event.title }}</p>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  data() {
+    return {
+      events: []
+    }
+  },
+  methods: {
+    fetchEvents() {
+      axios.get('http://127.0.0.1:8000/api/events')
+        .then(res => {
+          const events = res.data.results; // Accedi alla proprietà 'events'
+          this.events = events;
+        })
+        .catch(err => {
+          console.log(err)
+        })
+    }
+  },
+  mounted() {
+    this.fetchEvents()
+  }
+}
+</script>
+
+<style>
+</style>
