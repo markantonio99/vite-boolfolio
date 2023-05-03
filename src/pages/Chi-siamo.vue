5db401a2f099561d592726c69ef51edf541a96bd
“markantonio99” <markomakkia@gmail.com>
2023-05-03 21:28:43 +0200

    salv 0010

---
+++ src/pages/Chi-siamo.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <div class="">
+              <h1>Chi Siamo</h1>
+              <p>Lorem ipsum dolor sit amet.</p>
+        </div>
+    </div>
+
+    <div class="container">
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Temporibus, qui?</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Temporibus, qui?</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Temporibus, qui?</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Temporibus, qui?</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Temporibus, qui?</p>
+    </div>
+  
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
